07db96c28061144a918cfcb66d0af79bdbfb8952
neo-the-coder <kamranz150@gmail.com>
2024-01-28 20:54:50 +0400

    add: create.js solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,30 @@
+import { access, writeFile } from "node:fs/promises";
+import { URL } from "url";
+
 const create = async () => {
-    // Write your code here 
+  // Write your code here
+  const __dirname = new URL(".", import.meta.url).pathname;
+  const filePath = `${__dirname}files/fresh.txt`;
+  const fileContent = "I am fresh and young";
+
+  try {
+    // check if file exists
+    await access(filePath);
+    // throw an Error if the file exists
+    throw new Error("FS operation failed");
+  } catch (err) {
+    // if file missing error is thrown
+    if (err.code === "ENOENT") {
+      // create a file and write text to it
+      await writeFile(filePath, fileContent);
+      console.info(
+        "File has been created and the content has been written successfully"
+      );
+    } else {
+        // console log the error besides file missing
+        console.error(err);
+    }
+  }
 };
 
 await create();
