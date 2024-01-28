fbda5e52a5ee7720696815e6fcf3c24492e292b7
neo-the-coder <kamranz150@gmail.com>
2024-01-28 22:37:41 +0400

    add: list.js solution

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import { readdir } from "node:fs/promises";
+import { existsSync } from "node:fs";
+import { URL } from "url";
+
 const list = async () => {
-    // Write your code here 
+  // Write your code here
+  const path = new URL("./files", import.meta.url).pathname;
+
+  try {
+    // throw error if path does not exist
+    if (!existsSync(path)) {
+      throw new Error("FS operation failed");
+    }
+
+    const files = await readdir(path);
+    console.log(files);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await list();
