dd378c35886e1c015c19fdf493b4a3f873373206
neo-the-coder <kamranz150@gmail.com>
2024-01-28 22:03:16 +0400

    add: delete.js solution

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import { unlink } from "node:fs/promises";
+import { existsSync } from "node:fs";
+import { URL } from "url";
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  const __dirname = new URL(".", import.meta.url).pathname;
+  const fileToRemove = `${__dirname}files/fileToRemove.txt`;
+
+  try {
+    // check if the file does not exists
+    if (!existsSync(fileToRemove)) {
+      throw new Error("FS operation failed");
+    }
+
+    await unlink(fileToRemove);
+    console.log('"fileToRemove.txt" has been deleted successfully!');
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await remove();
