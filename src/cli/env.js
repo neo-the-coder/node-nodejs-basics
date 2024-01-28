bf947e9e87483b6ef14f74538b51948ae792f2d2
neo-the-coder <kamranz150@gmail.com>
2024-01-28 23:09:16 +0400

    add: env.js solution

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const PREFIX = "RSS_";
+  const variablesWithPrefix = Object.entries(process.env)
+    .filter(([key, value]) => key.startsWith(PREFIX))
+    .map(([key, value]) => `${key}=${value}`);
+
+  console.log(variablesWithPrefix.join("; "));
 };
 
 parseEnv();
