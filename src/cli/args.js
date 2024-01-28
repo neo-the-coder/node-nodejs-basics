94d224373a9b25ef0be6bcfa0d8d74f2b91762ba
neo-the-coder <kamranz150@gmail.com>
2024-01-28 23:34:48 +0400

    add: args.js solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const userArgs = process.argv.slice(2);
+  let result = [];
+
+  for (let i = 0; i < userArgs.length; i += 2) {
+    result.push(userArgs[i].replace(/--/, "") + " is " + userArgs[i + 1]);
+  }
+  console.log(result.join(", "));
 };
 
 parseArgs();
