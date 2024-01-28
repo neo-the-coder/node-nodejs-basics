9205ccb9152c75672d074b107e7f07c92a131bf2
neo-the-coder <kamranz150@gmail.com>
2024-01-28 22:44:47 +0400

    add: read.js solution

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import { readFile } from "node:fs/promises";
+import { existsSync } from "node:fs";
+import { URL } from "url";
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  const file = new URL("./files/fileToRead.txt", import.meta.url).pathname;
+
+  try {
+    // throw error if path does not exist
+    if (!existsSync(file)) {
+      throw new Error("FS operation failed");
+    }
+
+    const content = await readFile(file, "utf8");
+    console.log(content);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await read();
