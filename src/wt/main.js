80d14356aff708039602e50ac3f63ff7e8ea1a13
Kamran Zeynalov <kamran.zeynalov@aztelekom.az>
2024-01-29 16:30:07 +0400

    add: worker task unfinished solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,25 @@
+import { Worker } from "worker_threads";
+import os from "os";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 const performCalculations = async () => {
-    // Write your code here
+  // Write your code here
+  const cpuCores = os.cpus().length;
+  const workerFile = fileURLToPath(
+    path.join(import.meta.url, "..", "worker.js")
+  );
+  const result = [];
+
+  for (let i = 0; i < cpuCores; i++) {
+    const worker = new Worker(workerFile, { workerData: {num: i + 10} });
+    worker.on("error", (err) => console.log(err));
+    worker.on("message", (msg) => {
+      result.push(msg);
+    });
+    // worker.on('exit', () => {})
+  }
+  console.log(result);
 };
 
 await performCalculations();
